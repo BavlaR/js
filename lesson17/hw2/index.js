0357e95984bbf6c6926b791bf14863cdfdf16beb
BavlaR <vdmmailbox1@gmail.com>
2020-12-20 22:08:19 +0200

    new task2s

--- lesson17/hw2/index.js
+++ lesson17/hw2/index.js
@@ -1,3 +1,50 @@
+/* ===> 1 <=== */
+const student = {
+   name: 'Tom',
+};
+
+export function sayName() {
+   console.log(this.name);
+}
+
+// вызовите ф-цию sayName так, чтобы в консоль вывелось имя студента
+sayName.call(student);
+
+// вызовите ф-цию sayName так, чтобы в консоль вывелось имя 'Bruce' (используйте другой объект)
+sayName.call({ name: 'Bruce' });
+
+/* ===> 2 <=== */
+const company = {
+   companyName: 'Microsoft',
+};
+
+function greeting(firstName, lastName) {
+   console.log(`Hello, ${firstName} ${lastName}. Welcome to the ${this.companyName}`);
+}
+
+// вызовите ф-цию greeting так, чтобы в консоль вывелось
+// 'Hello, Bob Marley. Welcome to the Microsoft'
+// используйте объект company
+greeting.call(company, 'Bob', 'Marley');
+
+/* ===> 3 <=== */
+const country = {
+   countryName: 'Ukraine',
+   capital: 'Kyiv',
+};
+
+function getPopulation(population) {
+   return `Population in ${this.countryName} is ${population}`;
+}
+
+// вызовите ф-цию getPopulation так, чтобы она вернула
+// 'Population in Ukraine is 43000'
+// 43000 передавайте в виде числа
+// используйте объект country
+// результат работы ф-ции getPopulation присвойте в переменную и выведите в консоль
+getPopulation.call(country, 43000);
+
+/* ===> 4 <=== */
 const transaction = {
    amount: 1200,
    operation: 'sell',
