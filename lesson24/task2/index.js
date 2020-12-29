f5d641058ec15e7819ac7b2d14a766b74818d441
BavlaR <vdmmailbox1@gmail.com>
2020-12-29 22:08:28 +0200

    task2

---
+++ lesson24/task2/index.js
@@ -0,0 +1,9 @@
+function getGreenwichTime(date) {
+   const formatter = new Intl.DateTimeFormat('en', {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false,
+   });
+
+   return formatter.format(date);
+}
