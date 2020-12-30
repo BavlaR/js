aaf4912fed6989601d88500960ff00b1bb17ec43
BavlaR <vdmmailbox1@gmail.com>
2020-12-30 18:17:04 +0200

    hw1

--- lesson24/hw1/index.js
+++ lesson24/hw1/index.js
@@ -4,13 +4,13 @@ export function getDiff(startDate, endDate) {
    const days = Math.trunc(dateDiff / 86400000);
    dateDiff -= days * 86400000;
 
-   const hours = Math.trunc(dateDiff / 360000);
+   const hours = Math.trunc(dateDiff / 3600000);
    dateDiff -= hours * 360000;
 
-   const minutes = Math.trunc(dateDiff / 6000);
+   const minutes = Math.trunc(dateDiff / 60000);
    dateDiff -= minutes * 6000;
 
-   return `${days}d ${hours}h ${minutes}m ${Math.trunc(dateDiff / 100)}s`;
+   return `${days}d ${hours}h ${minutes}m ${Math.trunc(dateDiff / 1000)}s`;
 }
 
 console.log(getDiff(new Date(2016, 1), new Date(2015, 1)));
