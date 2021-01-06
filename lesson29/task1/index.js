fa5d1b13e8d2fea0e38e8faf2f3b64eef8bfe00e
BavlaR <vdmmailbox1@gmail.com>
2021-01-06 14:03:43 +0200

    task1

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -13,7 +13,7 @@ export const addImage = (imgSource, callback) => {
    };
 
    imgElem.addEventListener('load', onImageLoaded);
-   imgElem.addEventListener('error', () => callback('Image load fail'));
+   imgElem.addEventListener('error', () => callback('Image load is failed...'));
 };
 
 const onImageLoaded = (error, width, height) => {
@@ -25,3 +25,8 @@ const onImageLoaded = (error, width, height) => {
    const sizeElem = document.querySelector('.image-size');
    sizeElem.textContent = `${width} x ${height}`;
 };
+
+addImage(
+   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+   onImageLoaded,
+);
