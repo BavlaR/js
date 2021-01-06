6922cfdf9da4c83f0516c52150b9b1acbad852b7
BavlaR <vdmmailbox1@gmail.com>
2021-01-06 14:07:44 +0200

    task1

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -1,4 +1,4 @@
-export const addImage = (imgSource, callback) => {
+const addImage = (imgSource, callback) => {
    const imgElem = document.createElement('img');
    const containerElem = document.querySelector('.page');
 
@@ -26,7 +26,4 @@ const onImageLoaded = (error, width, height) => {
    sizeElem.textContent = `${width} x ${height}`;
 };
 
-addImage(
-   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-   onImageLoaded,
-);
+export { addImage };
