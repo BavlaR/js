ae746195f013eb3462dafc905e9eaeb19473a6de
BavlaR <vdmmailbox1@gmail.com>
2021-01-13 16:09:24 +0200

    task1

--- lesson32/task2/index.js
+++ lesson32/task2/index.js
@@ -1,4 +1,4 @@
-export const getRandomNumber = (from, to) => from + Math.random() * (to - from);
+const getRandomNumber = (from, to) => from + Math.random() * (to - from);
 
 const request = url =>
    new Promise(resolve => {
@@ -17,7 +17,7 @@ const request = url =>
 
 const servers = ['https://server.com/us', 'https://server.com/eu', 'https://server.com/au'];
 
-const getUserASAP = userId => {
+export const getUserASAP = userId => {
    const userUrls = servers.map(url => `${url}/users/${userId}`);
 
    const requests = userUrls.map(userUrl => request(userUrl));
