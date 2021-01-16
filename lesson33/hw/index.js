d4d4008c2e0984d593c5fad6ecee951aeeab9931
BavlaR <vdmmailbox1@gmail.com>
2021-01-16 09:28:49 +0200

    hw1

---
+++ lesson33/hw/index.js
@@ -0,0 +1,11 @@
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
+
+export function getTasksList() {
+   return fetch(baseUrl).then(response => response.json());
+}
+
+export function getTaskById(taskId) {
+   return fetch(baseUrl)
+      .then(response => response.json())
+      .then(data => data.find(oneData => oneData.id === taskId));
+}
