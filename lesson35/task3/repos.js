fa3ee994f5c2432736ed1a5a2448fba3870f6231
BavlaR <vdmmailbox1@gmail.com>
2021-01-19 19:43:52 +0200

    task3

---
+++ lesson35/task3/repos.js
@@ -0,0 +1,18 @@
+const list = document.querySelector('.repo-list');
+
+export const cleanReposList = () => {
+   list.textContent = '';
+};
+
+export const renderReposList = reposList => {
+   const reposListElems = reposList.map(({ name }) => {
+      const listElem = document.createElement('li');
+      listElem.classList.add('repo-list__item');
+      listElem.textContent = name;
+
+      return listElem;
+   });
+
+   cleanReposList();
+   list.append(...reposListElems);
+};
